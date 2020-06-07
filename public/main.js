498ee2ed861d22b80894d773e7581180560d1408
jaco9419 <jaco.9419@gmail.com>
2020-06-07 01:44:47 -0300

    Add files

---
+++ public/main.js
@@ -0,0 +1,36 @@
+// Information to reach API
+const apiKey = "d22cf0e897ca48059836f08ea4a1a45a";
+const url = "https://api.rebrandly.com/v1/links";
+
+// Some page elements
+const inputField = document.querySelector("#input");
+const shortenButton = document.querySelector("#shorten");
+const responseField = document.querySelector("#responseField");
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({ destination: urlToShorten });
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = "json";
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      renderResponse(xhr.response);
+    }
+  };
+  xhr.open("POST", url);
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.setRequestHeader("apikey", apiKey);
+  xhr.send(data);
+};
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while (responseField.firstChild) {
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+};
+
+shortenButton.addEventListener("click", displayShortUrl);
